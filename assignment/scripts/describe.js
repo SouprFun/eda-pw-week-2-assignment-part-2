197e208efe8586ea127c62c06473befe678d7276
Soup <bry123ftw@gmail.com>
2022-03-29 21:57:31 -0500

    question 5 complete

--- assignment/scripts/describe.js
+++ assignment/scripts/describe.js
@@ -150,6 +150,8 @@ if (mix === true) {
 let temp = 40;
 const time = 4;
 
+// FIX - the symbol || means OR, swap it for the && AND sign
+
 if (temp > 39 || time >= 4) {
   console.log('throw away the food!');
 }
